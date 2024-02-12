06fab1f67a5bb45271eb898bb2b754513e8ca687
IzawGithub <125356732+IzawGithub@users.noreply.github.com>
2024-02-12 17:38:15 +0100

    Add method cascading to missing func

--- src/main/java/com/parkit/parkingsystem/model/Ticket.java
+++ src/main/java/com/parkit/parkingsystem/model/Ticket.java
@@ -1,6 +1,5 @@
 package com.parkit.parkingsystem.model;
 
-import java.util.Calendar;
 import java.util.Date;
 
 public class Ticket {
@@ -15,24 +14,27 @@ public class Ticket {
         return id;
     }
 
-    public void setId(int id) {
+    public Ticket setId(int id) {
         this.id = id;
+        return this;
     }
 
     public ParkingSpot getParkingSpot() {
         return parkingSpot;
     }
 
-    public void setParkingSpot(ParkingSpot parkingSpot) {
+    public Ticket setParkingSpot(ParkingSpot parkingSpot) {
         this.parkingSpot = parkingSpot;
+        return this;
     }
 
     public String getVehicleRegNumber() {
         return vehicleRegNumber;
     }
 
-    public void setVehicleRegNumber(String vehicleRegNumber) {
+    public Ticket setVehicleRegNumber(String vehicleRegNumber) {
         this.vehicleRegNumber = vehicleRegNumber;
+        return this;
     }
 
     public double getPrice() {
