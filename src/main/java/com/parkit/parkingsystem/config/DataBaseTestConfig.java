194a6b10a175f3acb7b7a1341936dc92fcb773a1
IzawGithub <125356732+IzawGithub@users.noreply.github.com>
2024-02-12 17:32:22 +0100

    Add 2 database class: Production and test database

---
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseTestConfig.java
@@ -0,0 +1,18 @@
+package com.parkit.parkingsystem.config;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+public class DataBaseTestConfig extends AbstractDatabase {
+    public DataBaseTestConfig() throws IOException {
+        super(new SConfigFile());
+    }
+    public DataBaseTestConfig(SConfigFile config) throws IOException {
+        super(config);
+    }
+
+    @Override
+    public Connection getConnection() {
+        return super.getConnection("test");
+    }
+}
